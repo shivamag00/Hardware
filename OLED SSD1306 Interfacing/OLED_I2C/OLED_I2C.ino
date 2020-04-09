215feff3c6aaf7f49ef28faa05aa36a73e8c47bc
Shivam Agarwal <44562091+shivamag00@users.noreply.github.com>
2020-04-09 12:39:20 +0530

    Added info regarding displaying one box at a time

--- OLED SSD1306 Interfacing/OLED_I2C/OLED_I2C.ino
+++ OLED SSD1306 Interfacing/OLED_I2C/OLED_I2C.ino
@@ -1,12 +1,12 @@
 #include <Wire.h>
 
-//~ DEFINES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ DEFINES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Some defines for the SSD1306 controller driving a 128x64 resolution OLED display
 // PART             - http://www.simplelabs.co.in/content/96-blue-i2c-oled-module
 // DATASHEET        - https://www.adafruit.com/datasheets/SSD1306.pdf
 
-// The Slave Address (SLA) of the OLED controller - SSD1306 - is 0x3C 
-// The LSB is supposed to be the mode. Since we are only going to WRITE to the OLED, the LSB is going to be 0
+// The Slave Address (SLA) of the OLED controller - SSD1306 - is 0x3C (0b0111100)(b7-b1) 
+// The LSB of the address frame (b0) is supposed to be the mode. Since we are only going to WRITE to the OLED, the LSB is going to be 0
 // SLA (0x3C) + WRITE_MODE (0x00) =  0x78 (0b01111000)
 #define OLED_I2C_ADDRESS   0x3C
 
@@ -21,47 +21,50 @@
 
 #define OLED_CONTROL_BYTE_CMD_SINGLE      0x80
 #define OLED_CONTROL_BYTE_CMD_STREAM      0x00
+#define OLED_CONTROL_BYTE_DATA_SINGLE     0xC0
 #define OLED_CONTROL_BYTE_DATA_STREAM     0x40
 
 // Fundamental commands (pg.28)
-#define OLED_CMD_SET_CONTRAST             0x81  // follow with 0x7F
-#define OLED_CMD_DISPLAY_RAM              0xA4
-#define OLED_CMD_DISPLAY_ALLON                  0xA5
-#define OLED_CMD_DISPLAY_NORMAL                 0xA6
+#define OLED_CMD_SET_CONTRAST             0x81  // Double byte command - follow with a byte with value between 0x00 to  0x7F.
+#define OLED_CMD_DISPLAY_RAM              0xA4  // Displays RAM content
+#define OLED_CMD_DISPLAY_ALLON            0xA5  // Entire Display On - Output ignores RAM content. To know more, one must try it.
+#define OLED_CMD_DISPLAY_NORMAL           0xA6
 #define OLED_CMD_DISPLAY_INVERTED         0xA7
 #define OLED_CMD_DISPLAY_OFF              0xAE
-#define OLED_CMD_DISPLAY_ON                     0xAF
+#define OLED_CMD_DISPLAY_ON               0xAF
 
 // Addressing Command Table (pg.30)
 #define OLED_CMD_SET_MEMORY_ADDR_MODE     0x20  // follow with 0x00 = HORZ mode = Behave like a KS108 graphic LCD
 #define OLED_CMD_SET_COLUMN_RANGE         0x21  // can be used only in HORZ/VERT mode - follow with 0x00 + 0x7F = COL127
-#define OLED_CMD_SET_PAGE_RANGE                 0x22  // can be used only in HORZ/VERT mode - follow with 0x00 + 0x07 = PAGE7
+#define OLED_CMD_SET_PAGE_RANGE           0x22  // can be used only in HORZ/VERT mode - follow with 0x00 + 0x07 = PAGE7
 
 // Hardware Config (pg.31)
 #define OLED_CMD_SET_DISPLAY_START_LINE   0x40
 #define OLED_CMD_SET_SEGMENT_REMAP        0xA1  
-#define OLED_CMD_SET_MUX_RATIO                  0xA8  // follow with 0x3F = 64 MUX
+#define OLED_CMD_SET_MUX_RATIO            0xA8  // follow with 0x3F = 64 MUX
 #define OLED_CMD_SET_COM_SCAN_MODE        0xC8  
 #define OLED_CMD_SET_DISPLAY_OFFSET       0xD3  // follow with 0x00
 #define OLED_CMD_SET_COM_PIN_MAP          0xDA  // follow with 0x12
 
 // Timing and Driving Scheme (pg.32)
 #define OLED_CMD_SET_DISPLAY_CLK_DIV      0xD5  // follow with 0x80
-#define OLED_CMD_SET_PRECHARGE                  0xD9  // follow with 0x22
+#define OLED_CMD_SET_PRECHARGE            0xD9  // follow with 0x22
 #define OLED_CMD_SET_VCOMH_DESELCT        0xDB  // follow with 0x30
 
 // Charge Pump (pg.62)
 #define OLED_CMD_SET_CHARGE_PUMP          0x8D  // follow with 0x14
 
 // NOP
-#define OLED_CMD_NOP                            0xE3
+#define OLED_CMD_NOP                      0xE3
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+
+
 // Box pattern
 uint8_t pattern1[] = {
-      0x00,0x7E,0x42,0x42,0x42,0x42,0x7E,0x00,
-      0x00,0x7E,0x42,0x42,0x42,0x42,0x7E,0x00
+      0x00,0x00,0x2E,0x2A,0x3A,0x00,0x3E,0x08,
+      0x3E,0x00,0x3E,0x00,0x1E,0x20,0x1E,0x00
       };
 
 // Cross weave pattern
@@ -70,42 +73,7 @@ uint8_t pattern2[] = {
       0x81,0x42,0x24,0x18,0x18,0x24,0x42,0x81
       };
 
-void setup()   {                
-      // Init the OLED controller
-      OLED_init();
-}
 
-void loop() {
-      // I2C
-      Wire.beginTransmission(OLED_I2C_ADDRESS); //For the record, the parameter in Wire.beginTransmission specifies 7 bits (b7-b1). The last bit (b0) is made 0 by default. 
-                                                //For more info regarding that, you might search "7-bit addressing in I2C communications"
-      Wire.write(OLED_CONTROL_BYTE_CMD_STREAM);
-      Wire.write(OLED_CMD_SET_COLUMN_RANGE);
-      Wire.write(0x00);
-      Wire.write(0x7F);
-      Wire.write(OLED_CMD_SET_PAGE_RANGE);
-      Wire.write(0);
-      Wire.write(0x07);
-      Wire.endTransmission();
-
-      for(uint16_t i=0;i<1024;i++){
-            Wire.beginTransmission(OLED_I2C_ADDRESS);
-            Wire.write(OLED_CONTROL_BYTE_DATA_STREAM);
-          for (uint8_t x=0; x<16; x++) {  
-                  // Wire.write(0b11000001);    
-                  // Wire.write(0x81);    
-                  // Wire.write(0x02);
-                  Wire.write(pattern1[x]);
-                  // Wire.write(pattern2[x]);   
-                  i++;
-          }
-          i--;
-          Wire.endTransmission();   
-      }
-
-    delay(5000);
-      
-}
 
 
 void OLED_init() {
@@ -172,4 +140,64 @@ void OLED_init() {
 
       // End the I2C comm with the SSD1306
       Wire.endTransmission();
+}
+
+
+
+void setup()   {                
+      // Init the OLED controller
+      OLED_init();
+      // I2C
+      Wire.beginTransmission(OLED_I2C_ADDRESS); //For the record, the parameter in Wire.beginTransmission specifies 7 bits (b7-b1). The last bit (b0) is made 0 by default. 
+                                                //For more info regarding that, you might search "7-bit addressing in I2C communications"
+      Wire.write(OLED_CONTROL_BYTE_CMD_STREAM);
+      Wire.write(OLED_CMD_SET_COLUMN_RANGE);
+      Wire.write(0x00);
+      Wire.write(0x7F);
+      Wire.write(OLED_CMD_SET_PAGE_RANGE);
+      Wire.write(0);
+      Wire.write(0x07);
+      Wire.endTransmission();
+
+      for(uint16_t i=0;i<1024;i++){
+            Wire.beginTransmission(OLED_I2C_ADDRESS);
+            Wire.write(OLED_CONTROL_BYTE_DATA_STREAM);
+          for (uint8_t x=0; x<16; x++) {  
+                  // Wire.write(0b11000001);    
+                  // Wire.write(0x81);    
+                  // Wire.write(0x02);
+                  Wire.write(pattern1[x]);
+                  // Wire.write(pattern2[x]);   
+                  i++;
+          }
+          i--;
+          Wire.endTransmission();   
+      }
+
+    delay(5000);
+      
+}
+
+
+
+void loop() 
+{
+            delay(5000);  
+            for(uint16_t i=0;i<1024;i++)
+            {
+                  Wire.beginTransmission(OLED_I2C_ADDRESS);
+                  Wire.write(OLED_CONTROL_BYTE_DATA_STREAM);
+                  for (uint8_t x=0; x<16; x++) 
+                  {  
+                        // Wire.write(0b11000001);    
+                        // Wire.write(0x81);    
+                        // Wire.write(0x02);
+                        Wire.write(pattern2[x]);
+                        // Wire.write(pattern2[x]);   
+                        i++;
+                  }
+                  i--;
+                  Wire.endTransmission();
+                  delay(5000);  
+            }
 }
